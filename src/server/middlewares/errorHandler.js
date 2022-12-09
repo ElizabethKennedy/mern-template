f3f2880b92096ab628129bebfcd70f4d199cb974
Elizabeth Kennedy <110371718+ElizabethKennedy@users.noreply.github.com>
2022-12-09 08:20:12 -0800

    error handling

--- src/server/middlewares/errorHandler.js
+++ src/server/middlewares/errorHandler.js
@@ -1,5 +1,6 @@
 export const errorHandler = (err, req, res, next) => {
     console.error(err);
+
     res.status(err.status || 500).json({
       name: err.name || "Unknown error",
       msg: err.message || "An error occurred on the server.",
